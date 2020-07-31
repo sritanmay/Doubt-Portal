58713fed77f0af5acc231193491525d1cfd3670a
Tanmay <tanmayasrivastava99@gmail.com>
2020-08-01 02:34:12 +0530

    added Images, Footer Links, Description

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,7 +4,10 @@ import Signin from '../Signin/Signin';
 import Register from '../Register/Register';
 import Navigation from '../Navigation/Navigation';
 import Logo from '../Logo/Logo';
-import tanmay from '../../images/tanmay.png';
+import me from '../../images/me.jpg';
+import annotations from '../../images/annotations.jpg';
+import timetable from '../../images/timetable.jpg';
+import books from '../../images/books.jpg';
 import '../../App.css';
 
 class Home extends Component {
@@ -45,31 +48,31 @@ class Home extends Component {
                   <div><p className="f1" style={{ 'textAlign':'center', 'color':'white', 'marginTop':'50px'}}>About</p></div>
                   <div className="br2 pv3-m pv2-ns" style={{ 'display':'flex', 'alignItems':'center', 'background':'gray', 'margin':'20px 100px'}}>
                     <div className="v-mid w-50" style={{ 'textAlign':'center'}}>
-                      <img src="http://tachyons.io/img/super-wide.jpg" alt="A bright blue sky" className="mw5" />
+                      <img src={annotations} alt="Doubt" className="mw5" />
                     </div>
-                    <div className="v-mid pl3 w-50" style={{ 'textAlign':'center'}}>
+                    <div className="v-mid pl3 w-50 pr6" style={{ 'textAlign':'center'}}>
                       <p className="lh-copy">
-                        This text is vertically aligned middle, no matter what the height of the image is.
+                      Next time you have a doubt while studying, you know where to go. By simply posting your questions on our Doubt Forum, you can have them answered by academic experts.
                       </p>
                     </div>
                   </div>
                   <div className="br2 pv3-m pv2-ns" style={{ 'display':'flex', 'alignItems':'center', 'background':'gray', 'margin':'20px 100px'}}>
                     <div className="v-mid pl3 w-50" style={{ 'textAlign':'center'}}>
                       <p className="lh-copy">
-                        This text is vertically aligned middle, no matter what the height of the image is.
+                        Books...COMING SOON
                       </p>
                     </div>
                     <div className="v-mid w-50" style={{ 'textAlign':'center'}}>
-                      <img src="http://tachyons.io/img/super-wide.jpg" alt="A bright blue sky" className="mw5" />
+                      <img src={books} alt="A bright blue sky" className="mw5" />
                     </div>
                   </div>
                   <div className="br2 pv3-m pv2-ns" style={{ 'display':'flex', 'alignItems':'center', 'background':'gray', 'margin':'20px 100px'}}>
                     <div className="v-mid w-50" style={{ 'textAlign':'center'}}>
-                      <img src="http://tachyons.io/img/super-wide.jpg" alt="A bright blue sky" className="mw5" />
+                      <img src={timetable} alt="A bright blue sky" className="mw5" />
                     </div>
                     <div className="v-mid pl3 w-50" style={{ 'textAlign':'center'}}>
                       <p className="lh-copy">
-                        This text is vertically aligned middle, no matter what the height of the image is.
+                       Time-Tables...COMING SOON
                       </p>
                     </div>
                   </div>
@@ -79,13 +82,13 @@ class Home extends Component {
                 <div id="contact">{/*Contact section*/}
                   <div><p className="f1" style={{ 'textAlign':'center', 'color':'white', 'marginTop':'50px'}}>Contact</p></div>
                   <div className="br2 pv3-m pv2-ns" style={{ 'display':'flex', 'alignItems':'center', 'background':'gray', 'margin':'20px 100px'}}>
-                    <div className="v-mid pl3 w-50" style={{ 'textAlign':'center'}}>
+                    <div className="v-mid pl5 w-50" style={{ 'textAlign':'center'}}>
                       <p className="lh-copy">
-                        My name is Tanmaya Srivastava
+                      Hi, I'm Tanmaya Srivastava, a programmer/developer from Bhopal, India. I am currently doing my Bachelors in Computer Science & Engineering from National Institute of Technology, Bhopal.
                       </p>
                     </div>
                     <div className="v-mid w-50" style={{ 'textAlign':'center'}}>
-                      <img src={tanmay} alt="Tanmaya" className="mw5 br-100" />
+                      <img src={me} alt="Me" className="mw5 br-100" style={{'height':'auto'}} />
                     </div>
                   </div>
                 </div>
